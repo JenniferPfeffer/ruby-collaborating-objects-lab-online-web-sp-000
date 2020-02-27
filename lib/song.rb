b0dd02905cdfaee5ab4a515018bcaf0eaede9f1f
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-02-27 18:38:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,20 @@
-class Song 
-  attr_accessor :name, :artist 
-  @@all = [ ] 
-  def initialize(name) 
-    @name = name 
-    @@all << self 
-  end 
-  def self.all 
-    @@all 
-  end 
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
     new_song = self.new(song)
